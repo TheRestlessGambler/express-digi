d8d01e73eb01dc7fff0983a90c012f51cf15b7cd
TheRestlessGambler <abhinavratan.dev@gmail.com>
2024-07-08 04:31:54 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express'
 import 'dotenv/config'
+import logger from './logger.js';
+import morgan from 'morgan';
 
-
+const morganFormat = ':method :url :status :response-time ms';
 const app = express()
 const port = process.env.PORT||3000
+
+app.use(morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+  
+        };
+        logger.info(JSON.stringify(logObject));
+      }
+    }
+  }));
 app.use(express.json())
 
 let teaData = []
@@ -11,7 +28,7 @@ let nextId = 1
 
 //add a new item
 app.post('/teas', (req,res) =>{
-    
+    logger.warn("a post req has been made")
     const {name,price}= req.body
     const newTea = {id:nextId++, name, price}
     teaData.push(newTea)
