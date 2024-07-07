72c8f6f12e7d1c9391a1ec4727fa96a2641c07ab
TheRestlessGambler <abhinavratan.dev@gmail.com>
2024-07-07 20:14:47 +0530

    add to github

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+//add a new item
+app.post('/teas', (req,res) =>{
+    
+    const {name,price}= req.body
+    const newTea = {id:nextId++, name, price}
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+})
+//get all tea
+app.get('/teas',(req,res)=>{
+    res.status(200).send(teaData)
+})
+//get tea sorted by id
+app.get('/teas/:id',(req,res)=>{
+    const tea = teaData.find(t =>t.id === parseInt(req.params.id))
+    if(!tea){
+        return res.status(404).send('Not found')    
+    }
+    res.status(200).send(tea)
+})
+
+//update tea
+app.put('/teas/:id', (req,res)=>{
+    const teaId = req.params.id
+    const tea = teaData.find(t => t.id === parseInt(teaId))
+    if(!tea){
+        return res.status(404).send('Not found')    
+    }
+    const {name,price} = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+
+})
+
+//delete tea
+
+app.delete('/teas/:id',(req,res)=>{
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+
+    if(index === -1){
+        return res.status(404).send('Not found')
+    }
+    teaData.splice(index,1)
+    return res.status(204).send('item deleted')
+})
+
+app.listen(port, () =>{
+    console.log(`server is running at port:${port}`)
+})
+
